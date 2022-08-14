90072ec0b5886943495b30630fc73d7d01f82dfa
Макс <max.bondarev2005@gmail.com>
2022-08-14 19:27:40 +0300

    #

---
+++ phillkroger/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PhillkrogerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'phillkroger'
---
+++ phillkroger/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.mainapp, name="mainapp"),
+
+]
