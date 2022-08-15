f4e41a29affcd9ce800ce53ad5725db737439ddb
Макс <max.bondarev2005@gmail.com>
2022-08-15 11:43:01 +0300

    added-main-info

--- PhillKrogerBlogg/settings.py
+++ PhillKrogerBlogg/settings.py
@@ -12,16 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ns01ub@$+96iq8egm8283fgq$-pxs*)**8ycy%%mpn%9z(131'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
